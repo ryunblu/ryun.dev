5d933ab9a10ac7cc4c596d7853bbce629136382d
Ryan Scott <35160393+UnlitRyun@users.noreply.github.com>
2026-07-23 19:43:23 -0700

    feat: Redesigned header and footer
    Complete redesign with my logo at the bottom, the loading bar up top, and many more minor changes.

--- displayer.js
+++ displayer.js
@@ -1,9 +1,10 @@
 // Elements
 const startAnim = document.getElementById("start-anim");
-const HDDIcon2 = document.getElementById("hdd-icon-2")
-const main = document.getElementById("main");
+const HDDIcon2 = document.getElementById("hdd-icon-2");
 const header = document.getElementById("header");
 const leftNav = document.getElementById("left-nav");
+const main = document.getElementById("main");
+const footer = document.getElementById("footer");
 const aboutButton = document.getElementById("about-button");
 const backButton = document.getElementById("back-button");
 
@@ -11,26 +12,10 @@ aboutButton.onclick = () => {toggleMain()}
 backButton.onclick = () => {toggleMain()}
 
 // Reusable animations
-function animInOut(elem, animTime, startingScale) {
-    return elem.animate(
-        [
-            {opacity: 0, transform: `scale(${startingScale})`, easing: "steps(3, end)"},
-            {opacity: 1, transform: "scale(1)", offset: 0.1, ease: "easeIn"},
-            {opacity: 1, transform: "scale(1)", offset: 0.9, easing: "steps(2, end)"},
-            {opacity: 0, transform: "scale(0.9)"},
-        ],
-        {
-            duration: animTime,
-            fill: "forwards",
-        }
-    );
-}
 function animIn(elem, animTime, startingScale) {
     return elem.animate(
         [
             {opacity: 0, transform: `scale(${startingScale})`, easing: "steps(3, end)"},
-            {opacity: 1, transform: "scale(1)", offset: 0.1, ease: "easeIn"},
-            {opacity: 1, transform: "scale(1)", offset: 0.9, easing: "steps(2, end)"},
             {opacity: 1, transform: "scale(1)"},
         ],
         {
@@ -43,7 +28,7 @@ function animOut(elem, animTime, endingScale) {
     return elem.animate(
         [
             {opacity: 1, transform: "scale(1)", easing: "steps(3, end)"},
-            {opacity: 1, transform: `scale(${endingScale})`},
+            {opacity: 0, transform: `scale(${endingScale})`},
         ],
         {
             duration: animTime,
@@ -55,9 +40,10 @@ function animOut(elem, animTime, endingScale) {
 // Opening animation
 window.addEventListener('load', () => {
     let animTime = 2500;
-    
-    const anim = animInOut(startAnim, animTime, 0.8);
-    HDDIcon2.animate(
+
+    header.style.display = "flex";
+    animIn(header, 250, 0.9);
+    const anim = HDDIcon2.animate(
         [
             {opacity: 0},
             {opacity: 0, offset: 0.399},
@@ -76,19 +62,22 @@ window.addEventListener('load', () => {
 
     anim.finished.then(
         () => {
-            startAnim.style.display = "none";
             dealWithMobile();
+            showLeftNav();
+            toggleMain(true);
+            setTimeout(() => {footer.style.display = "block";}, 100);
+            animOut(startAnim, 200, 0.9)
             // Start listening to window resizing when done with opening anim
             window.addEventListener("resize", () => {dealWithMobile()})
         }
     );
 })
 
-// Show header w/left nav anim
-function showHeader() {
-    if (header.style.display === "none" || header.style.display === "") {
-        let animTime = 4500;
-        header.style.display = "block";
+// Show left-nav w/anim
+function showLeftNav() {
+    if (leftNav.style.display === "none" || leftNav.style.display === "") {
+        let animTime = 600;
+        leftNav.style.display = "flex";
         animIn(leftNav, animTime, 0.9);
         // Animate each child list item
         let waitTime = 40;
@@ -107,37 +96,21 @@ function isMobile() {
     return innerWidth < 600;
 }
 function dealWithMobile() {
-    if (main.style.display === "block") {
-        if (isMobile()) {
-            header.style.display = "none";
-            main.style.width = "100vw";
-        }
-        else {
-            showHeader();
-            main.style.width = "calc(100vw - 270px);";
-        }
-    }
-    else {
-        showHeader();
-        if (isMobile()) {
-            main.style.width = "100vw";
-        }
-        else {
-            main.style.width = "calc(100vw - 270px);";
-        }
-    }
+    // TODO: REMAKE
 }
 
 // Display main window or not
-function toggleMain() {
+function toggleMain(first = false) {
+    let animTime;
+    if (first) {animTime = 600}
+    else {animTime = 250}
+    
     if (main.style.display === "none" || main.style.display === "") {
         main.style.display = "block";
-        let animTime = 2500;
         animIn(main, animTime, 0.9);
         dealWithMobile();
     }
     else {
-        let animTime = 250;
         const anim = animOut(main, animTime, 0.9);
         anim.finished.then(() => {
             main.style.display = "none";
